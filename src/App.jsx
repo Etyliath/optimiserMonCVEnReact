041fb5e2821ebd218228be2ce7c77db6a422e0cc
Edouard DA SILVA <esdasilva@me.com>
2023-10-21 22:55:56 +0200

    create structure home page Issue #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
+import { Home } from "./pages/home"
 function App() {
   return (
     <>
-      <h1>Bonjour les gens!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        maiores optio ipsum, hic quasi incidunt nemo repellat, laborum illo
-        quaerat porro in nihil mollitia adipisci voluptatibus aut impedit, eum
-        consequuntur?
-      </p>
+      <Home />
     </>
   )
 }
