597eb318dc387487c83b8df1ef2c29ea9f4d7ed2
Edouard DA SILVA <esdasilva@me.com>
2023-10-23 18:55:48 +0200

    create page Realizations create component Project Issue #9

---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+export function Projects({ image, title, description, dev }) {
+  return (
+    <div
+      className='card'
+      style={{ width: '18rem' }}
+    >
+      <img
+        src={image}
+        className='card-img-top'
+        alt='...'
+      />
+      <div className='card-body'>
+        <h5 className=''>{title}</h5>
+        <p className=''>{description}</p>
+        <p style={{ fontStyle: 'italic' }}>{dev}</p>
+      </div>
+    </div>
+  )
+}
+Projects.propTypes = {
+  image: PropTypes.string,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  dev: PropTypes.string,
+}
