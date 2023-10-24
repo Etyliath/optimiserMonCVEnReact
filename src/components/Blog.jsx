82a848b4a95a8771d8dda1465c284e6cf70b774a
Edouard DA SILVA <esdasilva@hotmail.fr>
2023-10-24 13:01:41 +0200

    upadate design page blogs and component Blog Issue #13

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,18 +2,26 @@ import PropTypes from 'prop-types'
 
 export function Blog({ title, image, info, publication }) {
   return (
-    <article className='card col-lg-4 col-md-6' style={{ width: '20rem' }}>
-      <img
-          src={image}
-          className='card-img-top rounded-start'
-          alt='...'
-        />
-        <div className='card-body'>
-          <h5 className='card-title'>{title}</h5>
-          <p className='card-text'>{info}</p>
-          <p className='card-text'>
-            <small className='text-body-secondary'>{publication}</small>
-          </p>
+    <article
+      className='card col-6 p-2'
+      style={{ maxWidth: '360px' }}
+    >
+      <h5 className='card-title'>{title}</h5>
+      <div className=''>
+          <img
+            src={image}
+            className='img-fluid rounded-start'
+            alt='...'
+          />
+        </div>
+        <div className=''>
+          <div className='card-body p-2'>
+            
+            <p className='card-text'>{info}</p>
+            <p className='card-text'>
+              <small className='text-body-secondary'>{publication}</small>
+            </p>
+          </div>
         </div>
     </article>
   )
