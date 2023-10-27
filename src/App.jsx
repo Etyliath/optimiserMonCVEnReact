9ac3080763a7fe46dd16ffc56f02a317f6baf7be
Edouard DA SILVA <esdasilva@me.com>
2023-10-27 22:27:05 +0200

    create navigation install react-router-dom create component header create navbar Issue #21

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,73 @@
-import { Legales } from "./pages/Legales"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Legales } from './pages/Legales'
+import { Realizations } from './pages/Realizations'
+import { Blogs } from './pages/Blogs'
+import { Services } from './pages/Services'
+import { Contact } from './pages/Contact'
+import { Header } from './components/Header'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: '/services',
+    element: (
+      <>
+        <Header />
+        <Services />
+      </>
+    ),
+  },
+  {
+    path: '/realizations',
+    element: (
+      <>
+        <Header />
+        <Realizations />
+      </>
+    ),
+  },
+  {
+    path: '/blogs',
+    element: (
+      <>
+        <Header />
+        <Blogs />
+      </>
+    ),
+  },
+  {
+    path: '/contact',
+    element: (
+      <>
+        <Header />
+        <Contact />
+      </>
+    ),
+  },
+  {
+    path: '/legales',
+    element: (
+      <>
+        <Header />
+        <Legales />
+      </>
+    ),
+  },
+])
 
 function App() {
   return (
     <>
-      <Legales />
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,78 @@
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <>
+      <header >
+        <nav className='navbar navbar-expand-lg bg-primary' >
+          <div className='container-fluid' >
+            <NavLink
+              className='navbar-brand'
+              to='/'
+            >
+              John DOE
+            </NavLink>
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#navbarNav'
+              aria-controls='navbarNav'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div
+              className='collapse navbar-collapse text-uppercase'
+              id='navbarNav'
+            >
+              <ul className='navbar-nav ms-auto'>
+                <li className='nav-item'>
+                  <NavLink
+                    className='nav-link'
+                    to='/services'
+                  >
+                    Services
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink
+                    className='nav-link'
+                    to='/realizations'
+                  >
+                    Realisations
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink
+                    className='nav-link'
+                    to='/blogs'
+                  >
+                    Blogs
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink
+                    className='nav-link'
+                    to='/contact'
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink
+                    className='nav-link'
+                    to='/legales'
+                  >
+                    Mentions Légales
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
